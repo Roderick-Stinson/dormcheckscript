e9b20e7b067f5770890935353311d3c7e1539d4b
wangzihan <programmerroderickstinson@gmail.com>
2021-04-02 21:33:18 +0800

    更新了终端输出

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 import time
 
-url = str('https://www.wenjuan.com/s/UZBZJvG0Jy/')
+url = str('https://www.wenjuan.com/s/UZBZJvhuKX/#')
 t = int(10)
-score_list = [20, 13, 8, 10, 3, 7, 10, 10, 4, 3]
+# score_list = [20, 13, 8, 10, 3, 7, 10, 10, 4, 3]
+score_list = [5]
 # 设置提交问卷次数
-for times in range(501, 519):
+for times in range(501, 520):
     driver = webdriver.Firefox()
     driver.get(url)
     # 定位所有的问卷问题
@@ -20,10 +21,11 @@ for times in range(501, 519):
             blank_potion.send_keys(score_list[counter-1])
         counter += 1
 
-    print('{}班提交成功'.format(str(501 + int(counter))))
+    print('{}班提交成功'.format(str(times)))
     subumit_button = driver.find_element_by_css_selector('#next_button')
     subumit_button.click()
 
     # 延迟问卷结果提交时间，以免间隔时间太短而无法提交
     time.sleep(1)
     driver.quit()
+print('501-519班全部提交成功')
